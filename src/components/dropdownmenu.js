6aebb6791412cca4b459c8631e5df93a591b3544
Chris Gantz <chrisgantz836@gmail.com>
2018-08-29 11:15:09 -0700

    calc

---
+++ src/components/dropdownmenu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function dropDown(props) {
+  return (
+    <div>
+      <h1>Calendar</h1>
+      <form>
+        <label>What problem do you want to solve</label>
+        <select name="existAppt" id="existing">
+          <option>Hypotenuse for right triangle</option>
+          <option selected="selected">Quadratic Formula</option>
+        </select>
+      </form>
+    </div>
+  );
+}
