d6331480ba2d8a0fc70daa02e59490dd62767185
Chris Gantz <chrisgantz836@gmail.com>
2018-08-29 13:40:28 -0700

    added button

---
+++ src/components/formula-buttons.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PythagorasButton(props) {
+  return <button onClick={props.onClick}>Pythagorean Theorem</button>;
+}
--- src/components/solve.js
+++ src/components/solve.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import DropDown from './dropdownmenu';
-import Pythagoras from './pythagoras';
+import React from "react";
+import FormulaButton from "./formula-buttons";
+import Pythagoras from "./pythagoras";
 
 export default class Solve extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			display: 'Math'
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: "Math"
+    };
+  }
 
-	showPythagoras() {
-		this.setState({
-			display: 'Pythagoras'
-		});
-	}
+  showPythagoras() {
+    this.setState({
+      display: "Pythagoras"
+    });
+  }
 
-	render() {
-		if (this.state.display === 'Pythagoras') {
-			return <Pythagoras onClick={e => this.showPythagoras()} />;
-		} else {
-			return <DropDown />;
-		}
-	}
+  render() {
+    if (this.state.display === "Pythagoras") {
+      return <Pythagoras onClick={e => this.showPythagoras()} />;
+    } else if (this.state.display === "Math") {
+      return <FormulaButton />;
+    }
+  }
 }
