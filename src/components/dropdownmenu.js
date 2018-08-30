a06a3210cf894b09d32fae18f7c1323db062778e
Shelly Leung <pontificable@yahoo.com>
2018-08-29 19:03:03 -0700

    mentor helped get dropdown menu to work, coded quadratic to work. added dropdown menu to both pythag and quadratic form to return to "main menu" via the blank option

--- src/components/dropdownmenu.js
+++ src/components/dropdownmenu.js
@@ -3,16 +3,12 @@ import React from 'react';
 export default function dropDown(props) {
 	return (
 		<div>
-			<h1>Calculator</h1>
-			<form>
-				<label>What problem do you want to solve: </label>
-				<select name="Formula-list" id="formulas">
-					<option selected="selected" onClick={props.onClick}>
-						Pythagoras Theorem
-					</option>
-					<option>Quadratic Formula</option>
-				</select>
-			</form>
+			<h1>What Do You Want to Solve?</h1>
+			<select onChange={props.changeListener}>
+				<option value="Math"> </option>
+				<option value="pythagoream-theorum">Pythagorean Theorum</option>
+				<option value="quadratic-equation">Quadratic Equation</option>
+			</select>
 		</div>
 	);
 }
--- src/components/solve.js
+++ src/components/solve.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import FormulaButton from './formula-buttons';
 import Pythagoras from './pythagoras';
 import Hypotenuse from './pythag-output';
+import DropDown from './dropdownmenu';
 
 export default class Solve extends React.Component {
 	constructor(props) {
@@ -9,14 +9,16 @@ export default class Solve extends React.Component {
 		this.state = {
 			display: 'Formulas',
 			sideA: 3,
-			sideB: 4
+			sideB: 4,
+			sideC: 12
 		};
+
+		this.handleChange = this.handleChange.bind(this);
 	}
 
-	showPythagoras() {
-		this.setState({
-			display: 'Pythagoras'
-		});
+	handleChange(event) {
+		this.setState({ display: event.target.value });
+		console.log(event.target.value);
 	}
 
 	setSideA(sideA) {
@@ -27,8 +29,59 @@ export default class Solve extends React.Component {
 		this.setState({ sideB });
 	}
 
+	setSideC(sideC) {
+		this.setState({ sideC });
+	}
+
 	render() {
-		if (this.state.display === 'Pythagoras') {
+		if (this.state.display === 'quadratic-equation') {
+			//set the math variables with the values inside the inputs
+			const a = this.state.sideA;
+			const b = this.state.sideB;
+			const c = this.state.sideC;
+			//put the values into the quadratic equation and code Math operations to find x
+			// -b +- squarert(b^2 - 4ac) / 2a
+			const pt2 = Math.pow(b, 2);
+			const pt3 = 4 * a * c;
+			const square = Math.sqrt(Math.abs(pt2 - pt3));
+			const denominator = 2 * a;
+			const answer = (-b - square) / denominator;
+			const answer2 = (-b + square) / denominator;
+
+			return (
+				<form>
+					<Pythagoras
+						id="a"
+						label="a = "
+						min={-100}
+						max={100}
+						value={this.state.sideA}
+						onChange={value => this.setSideA(value)}
+					/>
+					<Pythagoras
+						id="b"
+						label="b = "
+						min={-100}
+						max={100}
+						value={this.state.sideB}
+						onChange={value => this.setSideB(value)}
+					/>
+					<Pythagoras
+						id="c"
+						label="c = "
+						min={-100}
+						max={100}
+						value={this.state.sideC}
+						onChange={value => this.setSideC(value)}
+					/>
+					<Hypotenuse id="x" label="x = " value={answer.toFixed(2)} />
+					<Hypotenuse id="x" label="or x = " value={answer2.toFixed(2)} />
+					<DropDown changeListener={this.handleChange} />
+				</form>
+			);
+		}
+
+		if (this.state.display === 'pythagoream-theorum') {
 			const answer = Math.sqrt(
 				Math.pow(this.state.sideA, 2) + Math.pow(this.state.sideB, 2)
 			);
@@ -37,7 +90,7 @@ export default class Solve extends React.Component {
 				<form>
 					<Pythagoras
 						id="side-a"
-						label="Side a"
+						label="Side a = "
 						min={0}
 						max={100}
 						value={this.state.sideA}
@@ -45,7 +98,7 @@ export default class Solve extends React.Component {
 					/>
 					<Pythagoras
 						id="side-b"
-						label="Side b"
+						label="Side b = "
 						min={0}
 						max={100}
 						value={this.state.sideB}
@@ -53,13 +106,18 @@ export default class Solve extends React.Component {
 					/>
 					<Hypotenuse
 						id="hypotenuse"
-						label="Side C"
+						label="Side c = "
 						value={answer.toFixed(2)}
 					/>
+					<DropDown changeListener={this.handleChange} />
 				</form>
 			);
 		} else {
-			return <FormulaButton onClick={e => this.showPythagoras()} />;
+			return (
+				<form>
+					<DropDown changeListener={this.handleChange} />
+				</form>
+			);
 		}
 	}
 }
